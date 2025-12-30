eb5dcf5db9f640fc7551d6fff4a8a4ad74d7ce79
SimramRFE <simran.rathour@rfetechnology.com>
2025-12-30 15:49:06 +0530

    new config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'www.gportalcms.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://gportalcms.com/api';
 
 // Create axios instance
 const api = axios.create({
